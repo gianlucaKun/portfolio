3663ca6373ea0a05b51b7a25bb7f60a834748c13
gianlucaKun <gianluca.andreoni95@gmail.com>
2024-07-25 14:55:19 +0200

    Revert "Update vite.config.ts"
    This reverts commit f5622d4b43373057979db7501ca0c4b52cdd869d.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-// vite.config.ts
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio' // Sostituisci 'nome-del-repository' con il nome del tuo repository GitHub
-});
+})
