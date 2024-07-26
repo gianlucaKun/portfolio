3743c2cdeab190f69e93a3344d646330a04e818d
gianlucaKun <gianluca.andreoni95@gmail.com>
2024-07-26 18:51:14 +0200

    update preview projects

--- src/components/cards/ProjectCard.tsx
+++ src/components/cards/ProjectCard.tsx
@@ -1,13 +1,23 @@
-import React from 'react'
-
+import React from "react";
+import "./ProjectCard.scss";
 interface CardProps {
-    imgUrl: string;
+  imgUrl: string;
+  text: string;
 }
 
-const ProjectCard: React.FC<CardProps> = ({ imgUrl }) => {
+const ProjectCard: React.FC<CardProps> = ({ imgUrl, text }) => {
   return (
-    <div><img src={imgUrl} alt="" /></div>
-  )
-}
+    <div className="containerCard">
+      <div className="card">
+        <img src={imgUrl} alt="Project Preview" />
+        <div className="text">
+          <p>{text}</p>
+          <button>Go to project!</button>
+        </div>
+        <div className="line"></div>
+      </div>
+    </div>
+  );
+};
 
-export default ProjectCard
+export default ProjectCard;
--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -3,16 +3,18 @@ import ProjectCard from '../cards/ProjectCard'
 import './Projects.scss'
 
 const Projects: React.FC = () => {
+
+  const testoprogetto1 : string = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque eum quod maiores at quidem culpa quasi eligendi ea, consequatur nemo animi et! '
   return (
     <div className='containerProjects'>
         <div><h2>Projects</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque eum quod maiores at quidem culpa quasi eligendi ea, consequatur nemo animi et! Unde illum eos itaque repellat at tempore ducimus.</p></div>
         <div className='containerPreviewProjects'>
-            <ProjectCard imgUrl='https://picsum.photos/400/300' />
-            <ProjectCard imgUrl='https://picsum.photos/400/300' />
-            <ProjectCard imgUrl='https://picsum.photos/400/300' />
-            <ProjectCard imgUrl='https://picsum.photos/400/300' />
-            <ProjectCard imgUrl='https://picsum.photos/400/300' />
+            <ProjectCard imgUrl='https://picsum.photos/400/300' text={testoprogetto1}/>
+            <ProjectCard imgUrl='https://picsum.photos/400/300' text="lorem ipsum casuale"/>
+            <ProjectCard imgUrl='https://picsum.photos/400/300' text="lorem ipsum casuale"/>
+            <ProjectCard imgUrl='https://picsum.photos/400/300' text="lorem ipsum casuale"/>
+            <ProjectCard imgUrl='https://picsum.photos/400/300' text="lorem ipsum casuale"/>
         </div>
     </div>
   )
