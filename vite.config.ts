ed0ed36303a05815cae2aed0ee6a0dbdfd47978f
gianlucaKun <gianluca.andreoni95@gmail.com>
2024-08-28 17:49:02 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // Sostituisci con il nome del tuo repository
+  base: '/portfolio', 
 })
