eaedcf3c3bf00081ee84672d37918719c23ec444
gianlucaKun <gianluca.andreoni95@gmail.com>
2024-09-09 10:43:56 +0200

    update preview projects

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,14 +1,14 @@
 import React, { useState } from 'react'
 import ProjectCard from '../cards/ProjectCard'
 import './Projects.scss'
-import veryvegpreview from '../../assets/veryvegpreview.jpg'
-import pizzeriapreview from '../../assets/pizzeriaPreview.png'
-import campingpreview from '../../assets/CampingLandingPage.png'
+import veryvegpreview from '../../assets/veryvegpreview2.jpg'
+import pizzeriapreview from '../../assets/pizzeriaPreview2.jpg'
+import campingpreview from '../../assets/CampingLandingPage2.jpg'
 
 const Projects: React.FC = () => {
   const [isHovered, setIsHovered] = useState(false);
 
-  const testoveryveg : string = 'Veryveg is a personal mobile application project designed to assist users in making informed dietary choices. The app allows users to select their initial diet type and then scan supermarket products to receive feedback based on their chosen dietary preferences. Additionally, it includes a checklist feature to help users keep track of their shopping list. The application is built using React Native for the frontend, with a backend powered by Java Spring Boot, and MongoDB is used as the database.'
+  const testoveryveg : string = "Veryveg è un'app mobile che aiuta gli utenti a fare scelte alimentari informate. Permette di selezionare il proprio regime dietetico, scansionare prodotti e ricevere feedback, includendo anche una checklist per la spesa. È sviluppata con React Native, Spring Boot e MongoDB."
 
   const testoPizzria : string = "Il progetto React per la pizzeria è un'applicazione web interattiva che offre una navigazione fluida attraverso diverse sezioni del sito, come la Home, il Menu, e una pagina About. Il progetto è organizzato utilizzando React Router per gestire le diverse pagine, fornendo un'esperienza utente dinamica."
 
